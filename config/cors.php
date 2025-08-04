64bd5bd7a79e4eb13685feea6bf3ad07a09532f5
dindagatya29 <dindagatya51@gmail.com>
2025-08-05 01:04:22 +0700

    Save local changes

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'https://nexapro.web.id'],
+    'allowed_origins' => ['https://nexapro.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
-];
+    'supports_credentials' => false,
+]; 
